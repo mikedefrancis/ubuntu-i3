b5703580b8a0251e81eb1fc3664c9f6ec5dbd191
mike <mike.defrancis@gmail.com>
2024-08-21 17:14:20 -0400

    adding installers and auto dependency installers for useful tools

--- capstone_test2.py
+++ capstone_test2.py
@@ -1,11 +1,23 @@
+import capstone
 from capstone import *
 
 # Initialize Capstone disassembler for ARM64
-md = Cs(CS_ARCH_ARM64, CS_MODE_ARM)
+md = Cs(CS_ARCH_ARM64, CS_MODE_LITTLE_ENDIAN)
 
 # Example code to disassemble
 code = b'\x00\x00\x80\xd2'
 
 for i in md.disasm(code, 0x1000):
     print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+
+
+# ARM64 machine code (in bytes)
+#  CODE2 = b"\x00\x80\x00\xd4"
+
+# Create a Capstone disassembler for ARM64
+#  md2 = Cs(CS_ARCH_ARM64, CS_MODE_LITTLE_ENDIAN)
+
+# Disassemble the code and print the results
+#  for instruction in md2.disasm(CODE2, 0x1000):
+#      print("0x%x:\t%s\t%s" % (instruction.address, instruction.mnemonic, instruction.op_str))
 
--- testkeystone.py
+++ testkeystone.py
@@ -3,3 +3,22 @@ from keystone import *
 ks = Ks(KS_ARCH_X86, KS_MODE_32)
 encoding, count = ks.asm("mov eax, 1")
 print("Encoded: %s" % encoding)
+# ARM64 assembly cod
+CODE = """
+    mov x0, #1
+    mov x1, #2
+    add x2, x0, x1
+"""
+
+# Initialize Keystone engine for ARM64
+ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN)
+
+# Assemble the code
+encoding, count = ks.asm(CODE)
+
+# Print the machine code
+print("Encoded machine code:")
+print(" ".join("{:02x}".format(byte) for byte in encoding))
+
+# Print the number of instructions assembled
+print("\nNumber of instructions: %u" % count)
